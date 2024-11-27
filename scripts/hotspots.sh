0f8a434c91daf6ca4e7c0f9c1e61b459d7cbee5c
andlin <andre@lindhjem.net>
2024-11-27 14:40:49 +0100

    Implement hotspots script that produces CSV output that describes hotspots in a repository.
    A command like `kca_hotspots` in a repository should now produce a
    CSV-file `.kca/hostspots.csv` which describes hotspots for a project.

--- scripts/hotspots.sh
+++ scripts/hotspots.sh
@@ -3,3 +3,8 @@ SCRIPT_DIR=$(dirname -- "$0";)
 
 MAAT_CMD="java -jar $SCRIPT_DIR/../code-maat-1.0.4-standalone.jar"
 ${MAAT_CMD} -l .kca/git_log.txt -c git2 -a revisions > .kca/revisions.csv
+
+CLOC_CMD="perl $SCRIPT_DIR/../cloc.pl"
+${CLOC_CMD} ./ --unix --by-file --csv --quiet --report-file=.kca/complexity.csv
+
+python3 "$SCRIPT_DIR/../maat-scripts-python3/merge/merge_comp_freqs.py" .kca/revisions.csv .kca/complexity.csv > .kca/hotspots.csv
