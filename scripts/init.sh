f5675064d2bbb5e687e891a932837cf2d0bb7bcf
Tobias Ørstad <tobias.orstad@nte.no>
2024-11-27 14:11:11 +0100

    endrer init scriptet til å git exclude opprettede filer

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash 
 
-echo steg 1: hent og lagre git loggen
+echo Steg 1: git exclude .kca mappen
 mkdir .kca
+grep '.kca/*' .git/info/exclude || echo -e "\n.kca/*\n" >> .git/info/exclude
+echo Steg 2: hent og lagre git loggen
 git log --all --numstat --date=short --pretty=format:'--%h--%ad--%aN' --no-renames > .kca/git_log.txt
 
 echo 4 Kjør maat summary
