c57b0cd6f85e2c35bad2313abed42673971409a6
Peter Norstein <peter.norstein@gmail.com>
2024-11-27 14:36:36 +0100

    Check if there exists an alias before registering it in setup

--- setup.sh
+++ setup.sh
@@ -4,6 +4,13 @@ echo lag .bashrc fil på brukerens hjemmappe
 echo den filen må inneholde kca alias
 absolute_path=$(pwd)
 echo $absolute_path
-echo "alias kca_init='$absolute_path/scripts/init.sh'" >> ~/.bashrc
-echo "alias kca_hotspots='$absolute_path/scripts/hotspots.sh'" >> ~/.bashrc
+# Add alias for kca_init if it doesn't exist
+if ! grep -q "alias kca_init=" ~/.bashrc; then
+    echo "alias kca_init='$absolute_path/scripts/init.sh'" >> ~/.bashrc
+fi
+
+# Add alias for kca_hotspots if it doesn't exist
+if ! grep -q "alias kca_hotspots=" ~/.bashrc; then
+    echo "alias kca_hotspots='$absolute_path/scripts/hotspots.sh'" >> ~/.bashrc
+fi
 source ~/.bashrc
