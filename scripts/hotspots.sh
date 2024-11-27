1bdabf1031b481c84b910f470d8b90855d583401
andlin <andre@lindhjem.net>
2024-11-27 14:22:03 +0100

    Basic implementation of hotspot command.

--- scripts/hotspots.sh
+++ scripts/hotspots.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash 
+SCRIPT_DIR=$(dirname -- "$0";)
 
-# TODO: Implement this
+MAAT_CMD="java -jar $SCRIPT_DIR/../code-maat-1.0.4-standalone.jar"
+${MAAT_CMD} -l .kca/git_log.txt -c git2 -a revisions > .kca/revisions.csv
