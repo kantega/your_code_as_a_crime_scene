9a90b2e1489550a68b30e3095fec8ab923d3a211
Peter Norstein <peter.norstein@gmail.com>
2024-11-27 14:50:11 +0100

    Use python or python3 depending on what is available

--- scripts/hotspots.sh
+++ scripts/hotspots.sh
@@ -7,4 +7,16 @@ ${MAAT_CMD} -l .kca/git_log.txt -c git2 -a revisions > .kca/revisions.csv
 CLOC_CMD="perl $SCRIPT_DIR/../cloc.pl"
 ${CLOC_CMD} ./ --unix --by-file --csv --quiet --report-file=.kca/complexity.csv
 
-python3 "$SCRIPT_DIR/../maat-scripts-python3/merge/merge_comp_freqs.py" .kca/revisions.csv .kca/complexity.csv > .kca/hotspots.csv
+# Check if python3 is available
+if command -v python3 &>/dev/null; then
+    PYTHON_CMD=python3
+# Check if python is available
+elif command -v python &>/dev/null; then
+    PYTHON_CMD=python
+else
+    echo "Error: Python is not installed."
+    exit 1
+fi
+
+# You can now use $PYTHON_CMD to run your Python scripts
+$PYTHON_CMD "$SCRIPT_DIR/../maat-scripts-python3/merge/merge_comp_freqs.py" .kca/revisions.csv .kca/complexity.csv > .kca/hotspots.csv
