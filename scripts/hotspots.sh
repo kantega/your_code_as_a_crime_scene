18904a0655ef78379cfe8d89be6cf20dbb70f9e1
Andreas Larsen <andreas.larsen@kantega.no>
2024-11-28 13:04:33 +0100

    hotspot: exlucde pom.xml and package-lock.json

--- scripts/hotspots.sh
+++ scripts/hotspots.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/bash 
+
+REPO_DIR=$(git rev-parse --show-toplevel)
+KCA_DIR="$REPO_DIR/.kca"
+
 SCRIPT_DIR=$(dirname -- "$0";)
+
 echo Steg 1 Genererer revisions fil
 MAAT_CMD="java -jar $SCRIPT_DIR/../code-maat-1.0.4-standalone.jar"
-${MAAT_CMD} -l .kca/git_log.txt -c git2 -a revisions > .kca/revisions.csv
+${MAAT_CMD} -l $KCA_DIR/git_log.txt -c git2 -a revisions > $KCA_DIR/revisions.csv
 echo Steg 2 Genererer kompleksitet fil
 CLOC_CMD="perl $SCRIPT_DIR/../cloc.pl"
-${CLOC_CMD} ./ --unix --by-file --csv --quiet --report-file=.kca/complexity.csv
+${CLOC_CMD} ./ --unix --by-file --csv --quiet --report-file=$KCA_DIR/complexity.csv
+
 
 # Check if python3 is available
 if command -v python3 &>/dev/null; then
@@ -19,6 +25,6 @@ else
 fi
 echo Steg 3 Slår sammen til hotspot fil
 # You can now use $PYTHON_CMD to run your Python scripts
-$PYTHON_CMD "$SCRIPT_DIR/../maat-scripts-python3/merge/merge_comp_freqs.py" .kca/revisions.csv .kca/complexity.csv | head > .kca/hotspots.csv
+$PYTHON_CMD "$SCRIPT_DIR/../maat-scripts-python3/merge/merge_comp_freqs.py" "$KCA_DIR/revisions.csv" "$KCA_DIR/complexity.csv" | grep -v 'pom.xml|package-lock.json' | head | tee "$KCA_DIR/hotspots.csv"
 echo Steg 4 Genererer graf
-$PYTHON_CMD "$SCRIPT_DIR/plotting/hotspot_plot.py" .kca/hotspots.csv .kca/hotspot_plot.png
+$PYTHON_CMD "$SCRIPT_DIR/plotting/hotspot_plot.py" "$KCA_DIR/hotspots.csv" "$KCA_DIR/hotspot_plot.png"
